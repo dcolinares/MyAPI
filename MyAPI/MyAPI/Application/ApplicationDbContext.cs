2fc48295fd67bfd1a5bb15836e5c064f4c8a3394
dcolinares <d.colinares@outlook.com>
2025-02-06 18:39:46 +0800

    Add database connection

---
+++ MyAPI/MyAPI/Application/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using DocumentFormat.OpenXml.Spreadsheet;
+using Microsoft.EntityFrameworkCore;
+using MyAPI.Model;
+using PundoPH.Model;
+using System;
+
+namespace MyAPI.Application
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Users> Users { get; set; } // Example entity
+        public DbSet<ContributionModel> Contributions { get; set; }
+        public DbSet<WithdrawModel> Withdraws { get; set; }
+    }
+}
--- MyAPI/MyAPI/Program.cs
+++ MyAPI/MyAPI/Program.cs
@@ -1,6 +1,14 @@
 using DocumentFormat.OpenXml.Office2016.Drawing.ChartDrawing;
+using Microsoft.EntityFrameworkCore;
+using MyAPI.Application;
 
 var builder = WebApplication.CreateBuilder(args);
+
+//Add DbContext
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 // Add services to the container.
 builder.Services.AddCors(options =>
